de5c816f71c56004cd7a755421bfad526143d165
anasyakubu <yakubuanas04@gmail.com>
2024-05-04 08:47:33 +0100

    bug fixes

--- app/Articles/Create/page.tsx
+++ app/Articles/Create/page.tsx
@@ -2,14 +2,31 @@
 import React, { useState } from "react";
 import "./page.scss";
 import Nav from "../../../components/shared/Nav";
+import { UploadDropzone } from "../../../utils/uploadthing";
 
 const Create = () => {
+  const [imageUrl, setImageUrl] = useState<string>("");
   return (
     <div className="Create">
       <div className="inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
         <Nav />
         <div className="p-20 lg:py-10">
-          <div className=""></div>
+          <div className="">
+            <UploadDropzone
+              className="bg-white text-black"
+              endpoint="imageUploader"
+              onClientUploadComplete={(res) => {
+                // Do something with the response
+                console.log("Files: ", res);
+                setImageUrl(res[0].url);
+                alert("Upload Completed");
+              }}
+              onUploadError={(error: Error) => {
+                // Do something with the error.
+                alert(`ERROR! ${error.message}`);
+              }}
+            />
+          </div>
           <div className="mt-5">
             <div className="space-y-2 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
               <div className="">
